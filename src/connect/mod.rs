5a7a1e9eec3556c9f142192ed2dfe27214c0cc4f
Violet <lucaswoodbusiness@gmail.com>
2021-11-19 16:20:33 -0800

    refactoring and QoL
    Signed-off-by: Violet <lucaswoodbusiness@gmail.com>

--- src/connect/mod.rs
+++ src/connect/mod.rs
@@ -9,18 +9,38 @@ use std::borrow::Cow;
 
 use either::Either;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-#[derive(Debug, Serialize, Deserialize)]
+// TODO: Document these especially!
+
+pub type TopicOrStr<'a> = Either<Topic<'a>, &'a str>;
+pub type EventTypeOrStr<'a> = Either<EventType, &'a str>;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Event<'a, 'b, T> {
-    #[serde(borrow)]
-    topic: Either<Topic<'a>, &'a str>,
-    #[serde(borrow)]
-    event: Either<EventType, &'b str>,
+    #[serde(with = "either::serde_untagged", borrow)]
+    topic: TopicOrStr<'a>,
+    #[serde(with = "either::serde_untagged", borrow)]
+    event: EventTypeOrStr<'b>,
     payload: T,
-    reference: uuid::Uuid,
+    #[serde(rename = "ref")]
+    reference: Uuid,
 }
 
-impl<'a, 'b, T> Event<'a, 'b, T> {}
+impl<'a, 'b, T> Event<'a, 'b, T> {
+    pub fn new(topic: TopicOrStr<'a>, event: EventTypeOrStr<'b>, payload: T) -> Self {
+        Event {
+            topic,
+            event,
+            payload,
+            reference: Uuid::new_v4(),
+        }
+    }
+
+    pub fn is_reply_to(&self, other: &Self) -> bool {
+        self.reference == other.reference
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
@@ -32,7 +52,7 @@ pub enum Topic<'a> {
     Room(Cow<'a, str>),
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum EventType {
     Heartbeat,
