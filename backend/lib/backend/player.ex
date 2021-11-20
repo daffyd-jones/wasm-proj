fa50926495af555860151e34859d99ce47e253e0
jeff-pancho <jepoypancho@hotmail.com>
2021-11-19 16:46:45 -0800

    Added state for Player processes. Added 'inspect' and 'update_pos' events

--- backend/lib/backend/player.ex
+++ backend/lib/backend/player.ex
@@ -1,27 +1,53 @@
 defmodule Backend.Player do
   use GenServer
+  alias BackendWeb.Endpoint
 
+  # Structs are named after the module they are in
+  # so we have to create another module
+  defmodule State do
+    @derive {Jason.Encoder, only: [:uuid, :pos, :lives]}
+    defstruct uuid: nil,
+              pos: %{x: 0, y: 0},
+              lives: 3
+  end
+
+  ### Public API ###
+  
+  def start_link(uuid) do
+    GenServer.start_link(__MODULE__, uuid, name: name(uuid))
+  end
+  
   defp name(uuid) do
     {:global, uuid}
   end
-  
-  def do_something_funny(uuid) do
-    GenServer.cast(name(uuid), :do_something_funny)
-  end
-  
-  def start_link(uuid) do
-    # TODO: wtf do i put for state??? lol
-    GenServer.start_link(__MODULE__, uuid, name: name(uuid))
+
+  def inspect(uuid) do
+    GenServer.call(name(uuid), :inspect)
   end
 
+  def update_pos(uuid, new_pos) do
+    GenServer.cast(name(uuid), {:update_pos, new_pos})
+  end
+
+  ### GenServer ###
+  
+  @impl true
   def init(uuid) do
-    IO.puts("I AM ALIVE #{uuid}")
-    {:ok, uuid}
+    state = %State{uuid: uuid}
+    IO.puts("New player process created with UUID #{uuid}.")
+    
+    {:ok, state}
   end
   
   @impl true
-  def handle_cast(:do_something_funny, state) do
-    IO.puts("Doing something funny!!! #{state}")
-    {:noreply, state}
+  def handle_call(:inspect, _from, state) do
+    {:reply, state, state}
+  end
+
+  @impl true
+  def handle_cast({:update_pos, %{"x" => x, "y" => y}}, state) do
+    new_state = %{state | pos: %{x: x, y: y}}
+    Endpoint.broadcast("room:lobby", "new_pos", new_state)
+    {:noreply, new_state}
   end
 end
