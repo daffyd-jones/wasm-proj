632f02abee73a34b4ed0721cb667f797bc25703c
jeff-pancho <jepoypancho@hotmail.com>
2021-11-23 23:18:01 -0800

    JSON parser properly handles Backend.State.Grids struct

--- backend/lib/backend/grid.ex
+++ backend/lib/backend/grid.ex
@@ -10,4 +10,17 @@ defmodule Backend.Grid do
   def set(grid, x, y, val) do
     put_in(grid[x][y], val)
   end
+  
+  def to_list_remove_keys(map) do
+    map
+    |> Enum.to_list()
+    |> Enum.map(fn {_, x} -> x end)
+  end
+  
+  # Warning: After a certain size, maps become unordered
+  def to_list(grid) do
+    grid
+    |> to_list_remove_keys()
+    |> Enum.map(&to_list_remove_keys/1)
+  end
 end
